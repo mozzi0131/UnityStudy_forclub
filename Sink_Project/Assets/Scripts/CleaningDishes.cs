17b6e102910808fd645318629279dc33f7d48173
mozzi0131 <sue03094@gmail.com>
2017-07-19 16:54:58 +0900

    add code
    code for cleaning dishes

---
+++ Sink_Project/Assets/Scripts/CleaningDishes.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleaningDishes : MonoBehaviour {
+
+    int[] cleannum = new int[3];
+    GameObject[] cleanObj = new GameObject[3];
+    int nowobjindex;
+
+    void Start()
+    {
+        cleannum[0] = 3;
+        cleannum[1] = 4;
+        cleannum[2] = 5;
+
+        cleanObj[0] = GameObject.Find("Plate");
+        cleanObj[1] = GameObject.Find("CuttingBoard");
+        cleanObj[2] = GameObject.Find("Spoon");
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(other.gameObject.name);
+            switch (other.gameObject.name)
+            { 
+                case "Plate":
+                    nowobjindex = 0;
+                    cleannum[0]--;
+                    break;
+                case "CuttingBoard":
+                    nowobjindex = 1;
+                    cleannum[1]--;
+                    break;
+                case "Spoon":
+                    nowobjindex = 2;
+                    cleannum[2]--;
+                    break;
+            }
+
+            if (cleannum[nowobjindex] == 0)
+            {
+                gameObject.GetComponent<PlayerMoving>().killObj[nowobjindex] = true;
+                cleanObj[nowobjindex].SetActive(false);
+            }
+                
+        }
+         
+    }
+}
--- Sink_Project/Assets/Scripts/PlayerMoving.cs
+++ Sink_Project/Assets/Scripts/PlayerMoving.cs
@@ -5,39 +5,43 @@ using UnityEngine;
 public class PlayerMoving : MonoBehaviour {
 
     float playerspeed = 10f;
+    public bool[] killObj = new bool[3];
 
     public bool killPlate = false;
     public bool killCuttingboard = false;
     public bool killSpoon = false;
+
 	// Use this for initialization
 	void Start () {
-		
+        for(int i = 0; i < 3; i++)
+        {
+            killObj[i] = false;
+        }
+        
 	}
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Debug.Log("left");
             if (gameObject.transform.position.x >= -8)
                 gameObject.transform.Translate(-playerspeed * Time.deltaTime, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            Debug.Log("right");
             if (gameObject.transform.position.x <= 8)
             {
                 gameObject.transform.Translate(playerspeed * Time.deltaTime, 0, 0);
-                if(killPlate == false && gameObject.transform.position.x >= -4.79f)
+                if(killObj[0] == false && gameObject.transform.position.x >= -4.79f)
                 {
                     gameObject.GetComponent<Transform>().position = new Vector3(-4, gameObject.transform.position.y, 0);
                 }
-                else if (killCuttingboard==false && gameObject.transform.position.x >= -0.9f)
+                else if (killObj[1] == false && gameObject.transform.position.x >= -0.9f)
                 {
                     gameObject.GetComponent<Transform>().position = new Vector3(-0.9f, gameObject.transform.position.y, 0);
                 }
-                else if(killSpoon == false && gameObject.transform.position.x >= 2.7f)
+                else if(killObj[2] == false && gameObject.transform.position.x >= 2.7f)
                 {
                     gameObject.GetComponent<Transform>().position = new Vector3(2.7f, gameObject.transform.position.y, 0);
                 }
@@ -45,9 +49,9 @@ public class PlayerMoving : MonoBehaviour {
                 
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("space");
+            
             gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerspeed * 50);
         }
             
